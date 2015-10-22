d4c67f31f5851fb05391d5f3d767e0caf1e11b5c
Egor Yusov <egor.yusov@gmail.com>
2015-10-21 19:59:12 -0700

    Updated Android configuration

--- External/libtiff-4.0.3/build/Windows/jni/Android.mk
+++ External/libtiff-4.0.3/build/Windows/jni/Android.mk
@@ -20,6 +20,7 @@ LOCAL_C_INCLUDES := $(PROJECT_ROOT)/libtiff
 LOCAL_SRC_FILES := ../../../libtiff/tif_aux.c ../../../libtiff/tif_close.c ../../../libtiff/tif_codec.c ../../../libtiff/tif_color.c ../../../libtiff/tif_compress.c ../../../libtiff/tif_dir.c ../../../libtiff/tif_dirinfo.c ../../../libtiff/tif_dirread.c ../../../libtiff/tif_dirwrite.c ../../../libtiff/tif_dumpmode.c ../../../libtiff/tif_error.c ../../../libtiff/tif_extension.c ../../../libtiff/tif_fax3.c ../../../libtiff/tif_fax3sm.c ../../../libtiff/tif_flush.c ../../../libtiff/tif_getimage.c ../../../libtiff/tif_jbig.c ../../../libtiff/tif_jpeg.c ../../../libtiff/tif_jpeg_12.c ../../../libtiff/tif_luv.c ../../../libtiff/tif_lzma.c ../../../libtiff/tif_lzw.c ../../../libtiff/tif_next.c ../../../libtiff/tif_ojpeg.c ../../../libtiff/tif_open.c ../../../libtiff/tif_packbits.c ../../../libtiff/tif_pixarlog.c ../../../libtiff/tif_predict.c ../../../libtiff/tif_print.c ../../../libtiff/tif_read.c ../../../libtiff/tif_stream.cxx ../../../libtiff/tif_strip.c ../../../libtiff/tif_swab.c ../../../libtiff/tif_thunder.c ../../../libtiff/tif_tile.c ../../../libtiff/tif_unix.c ../../../libtiff/tif_version.c ../../../libtiff/tif_warning.c ../../../libtiff/tif_win32.c ../../../libtiff/tif_write.c ../../../libtiff/tif_zip.c ../../../port/lfind.c
 
 #VisualGDBAndroid: VSExcludeListLocation
+VISUALGDB_VS_EXCLUDED_FILES_Release :=../../../libtiff/tif_win32.c
 VISUALGDB_VS_EXCLUDED_FILES_Debug := ../../../libtiff/tif_win32.c
 LOCAL_SRC_FILES := $(filter-out $(VISUALGDB_VS_EXCLUDED_FILES_$(VGDB_VSCONFIG)),$(LOCAL_SRC_FILES))
 
