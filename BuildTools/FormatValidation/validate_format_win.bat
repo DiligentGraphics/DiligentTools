c3bc561843647a25f2ef8041984f13c851294cd8
assiduous <assiduous@diligentgraphics.com>
2019-12-19 18:55:41 -0800

    Renamed UintTests folder to Tests

--- BuildTools/FormatValidation/validate_format_win.bat
+++ BuildTools/FormatValidation/validate_format_win.bat
@@ -1,6 +1,6 @@
 python ../../../DiligentCore/BuildTools/FormatValidation/clang-format-validate.py --color never ^
 --clang-format-executable ../../../DiligentCore/BuildTools/FormatValidation/clang-format_10.0.0.exe ^
--r ../../AssetLoader ../../Imgui ../../NativeApp/include  ../../NativeApp/src ../../TextureLoader ^
+-r ../../AssetLoader ../../Imgui ../../NativeApp/include  ../../NativeApp/src ../../TextureLoader ../../Tests ^
 --exclude ../../Imgui/interface/ImGuiImplMacOS.h ^
 --exclude ../../Imgui/interface/ImGuiImplIOS.h ^
 --exclude ../../NativeApp/src/UWP ^
