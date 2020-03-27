7f87356ea591da6ba5b5eab3514bcd6b33b5244c
assiduous <assiduous@diligentgraphics.com>
2020-03-26 19:53:37 -0700

    Enabled format validation on linux

--- BuildTools/FormatValidation/validate_format_linux.sh
+++ BuildTools/FormatValidation/validate_format_linux.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-echo "Format validation is not yet enabled for DiligentTools module on Linux"
-
+python ../../../DiligentCore/BuildTools/FormatValidation/clang-format-validate.py \
+--clang-format-executable ../../../DiligentCore/BuildTools/FormatValidation/clang-format_linux_10.0.0 \
+-r ../../AssetLoader ../../Imgui ../../NativeApp/include  ../../NativeApp/src ../../TextureLoader ../../Tests \
+--exclude ../../Imgui/interface/ImGuiImplMacOS.h \
+--exclude ../../Imgui/interface/ImGuiImplIOS.h \
+--exclude ../../NativeApp/src/UWP \
+--exclude ../../NativeApp/include/UWP
--- BuildTools/FormatValidation/validate_format_mac.sh
+++ BuildTools/FormatValidation/validate_format_mac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python ../../../DiligentCore/BuildTools/FormatValidation/clang-format-validate.py \
---clang-format-executable ../../../DiligentCore/BuildTools/FormatValidation/clang-format_10.0.0 \
+--clang-format-executable ../../../DiligentCore/BuildTools/FormatValidation/clang-format_mac_10.0.0 \
 -r ../../AssetLoader ../../Imgui ../../NativeApp/include  ../../NativeApp/src ../../TextureLoader ../../Tests \
 --exclude ../../Imgui/interface/ImGuiImplMacOS.h \
 --exclude ../../Imgui/interface/ImGuiImplIOS.h \
