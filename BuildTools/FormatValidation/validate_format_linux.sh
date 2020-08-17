290098e1590b84472ec2ceff9a91a87e40f06b6a
assiduous <assiduous@diligentgraphics.com>
2020-08-16 20:50:26 -0700

    Allow usage of system clang-format instead of bundled (https://github.com/DiligentGraphics/DiligentEngine/issues/79)

--- BuildTools/FormatValidation/validate_format_linux.sh
+++ BuildTools/FormatValidation/validate_format_linux.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-python ../../../DiligentCore/BuildTools/FormatValidation/clang-format-validate.py \
---clang-format-executable ../../../DiligentCore/BuildTools/FormatValidation/clang-format_linux_10.0.0 \
--r ../../AssetLoader ../../Imgui ../../NativeApp/include  ../../NativeApp/src ../../TextureLoader ../../Tests \
+
+source ../../../DiligentCore/BuildTools/FormatValidation/validate_format_linux_implementation.sh
+
+validate_format ../../AssetLoader ../../Imgui ../../NativeApp/include  ../../NativeApp/src ../../TextureLoader ../../Tests \
 --exclude ../../Imgui/interface/ImGuiImplMacOS.h \
 --exclude ../../Imgui/interface/ImGuiImplIOS.h \
 --exclude ../../NativeApp/src/UWP \
