678cb07b1d627bff80308ac421b433dc941f91b9
MikhailGorobets <misha.friman@mail.ru>
2021-12-01 20:55:08 +0500

    Render State Notation: Added format validation

--- BuildTools/FormatValidation/validate_format_mac.sh
+++ BuildTools/FormatValidation/validate_format_mac.sh
@@ -2,7 +2,9 @@
 python ../../../DiligentCore/BuildTools/FormatValidation/clang-format-validate.py \
 --clang-format-executable ../../../DiligentCore/BuildTools/FormatValidation/clang-format_mac_10.0.0 \
 -r ../../AssetLoader ../../Imgui ../../NativeApp/include  ../../NativeApp/src ../../TextureLoader ../../Tests \
+../../RenderStateNotationParser ../../RenderStatePackager \
 --exclude ../../Imgui/interface/ImGuiImplMacOS.h \
 --exclude ../../Imgui/interface/ImGuiImplIOS.h \
 --exclude ../../NativeApp/src/UWP \
---exclude ../../NativeApp/include/UWP
+--exclude ../../NativeApp/include/UWP \
+--exclude ../../RenderStateNotationParser/generated
