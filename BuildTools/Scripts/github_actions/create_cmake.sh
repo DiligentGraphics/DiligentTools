18082474105cc89a61ef07139ba768dfa79bb2ca
assiduous <assiduous@diligentgraphics.com>
2022-04-26 23:14:38 -0700

    GitHub actions: use DiligentGraphics/github-action/configure-cmake

--- BuildTools/Scripts/github_actions/create_cmake.sh
+++
@@ -1,5 +0,0 @@
-cd $1
-echo "cmake_minimum_required(VERSION 3.6)" > CMakeLists.txt
-echo "Project(DiligentTools_Test)" >> CMakeLists.txt
-echo "add_subdirectory(DiligentCore)" >> CMakeLists.txt
-echo "add_subdirectory(DiligentTools)" >> CMakeLists.txt
